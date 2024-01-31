fd1f034ca5fb70ec72ce609879fc24f5f4ce3b59
Jeanvally G.Beato <jeanvallybeato@gmail.com>
2024-01-30 23:21:08 -0800

    Display & operators work.

--- javascript.js
+++ javascript.js
@@ -1,42 +1,91 @@
-let firstNumber;
-let secondNumber;
-let operator;
+const buttons = document.querySelectorAll('button');
+const calcDisplay = document.getElementById('calcDisplay');
+
+let firstNumber = '';
+let secondNumber = '';
+let operator = '';
+
+function updateCalcDisplay() {
+  calcDisplay.textContent = firstNumber + operator + secondNumber;
+}
+
+buttons.forEach((button) => {
+  button.addEventListener('click', ()=> {
+    const value = button.dataset.value;
+
+    //1 - Check if button clicked is number.  If a # & no operator yet, update 1, otherwise update 2.
+    if(!isNaN(value)) {
+      if(!operator){
+        firstNumber += value;
+      }
+      else {
+        secondNumber += value;
+      }
+    }
+
+    //2 - If operator selected, check if 1/2/op already exist, store operate() in 1.  Reset 2.  Store new op.
+    //RESOURCE: https://stackoverflow.com/questions/4728144/check-variable-equality-against-a-list-of-values
+    if (['+', '-', '*', '/',].includes(value)) {
+      if(operator){
+        firstNumber = operate(operator,parseInt(firstNumber),parseInt(secondNumber));
+        secondNumber = '';
+      }
+      operator = value;
+    }
+
+    //3 - If = selected & 1/2/op exist, store operate() in 1.  Reset 2. Reset op.
+    if(value === '='){
+      if(firstNumber && operator && secondNumber){
+        firstNumber = operate(operator,parseInt(firstNumber),parseInt(secondNumber));
+        secondNumber = '';
+        operator = '';
+      }
+    }
+
+    //4 - If clear selected, clear out 1/2/op
+    if(value === 'clear'){
+      firstNumber = '';
+      secondNumber = '';
+      operator = '';
+    }
+
+    updateCalcDisplay();
+
+  });
+});
+
 
 function operate(operator, firstNumber, secondNumber){
 
     switch (operator) {
         case "+":
-          add(firstNumber, secondNumber);
-          break;
+          return add(firstNumber, secondNumber);
+  
         case "-":
-          subtract(firstNumber, secondNumber);
-          break;
+          return subtract(firstNumber, secondNumber);
+
         case "*":
-          multiply(firstNumber, secondNumber);
-          break;
+          return multiply(firstNumber, secondNumber);
+
         case "/":
-          divide(firstNumber, secondNumber);
-          break;
+          return divide(firstNumber, secondNumber);
+
       }
 
 };
 
 function add(firstNumber, secondNumber){
-    let answer = firstNumber + secondNumber;
-    return answer;
+    return firstNumber + secondNumber;
 };
 
 function subtract(firstNumber, secondNumber){
-    let answer = firstNumber - secondNumber;
-    return answer;
+    return firstNumber - secondNumber;
 };
 
 function multiply(firstNumber, secondNumber){
-    let answer = firstNumber * secondNumber;
-    return answer;
+    return firstNumber * secondNumber;
 };
 
 function divide(firstNumber, secondNumber){
-    let answer = firstNumber / secondNumber;
-    return answer;
+    return firstNumber / secondNumber;
 };
